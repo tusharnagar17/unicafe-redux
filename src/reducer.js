80374cd23bd4f092a8b05a1090b1e369635917a2
Tushar <nagartushar771@gmail.com>
2024-01-10 10:46:39 +0530

    added reducer logic

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,26 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
-  }
-  
-}
+    case "GOOD":
+      return { ...state, good: state.good + 1 };
+    case "OK":
+      return { ...state, ok: state.ok + 1 };
 
-export default counterReducer
+    case "BAD":
+      return { ...state, bad: state.bad + 1 };
+
+    case "ZERO":
+      return initialState;
+    default:
+      return state;
+  }
+};
+// const store = createStore(counterReducer);
+
+export default counterReducer;
